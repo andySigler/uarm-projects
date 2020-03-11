04e19173ffc99ac8fad97bb12c7559162342462a
andySigler <andrewsigler1@gmail.com>
2020-03-10 22:43:18 -0400

    uses system avrdude

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -6,9 +6,8 @@ FIRMWARE_DIR="$(
   cd "$(dirname "$0")" || exit
   pwd -P
 )"
-AVRDUDE_PATH="/Applications/Arduino-1.8.11.app/Contents/Java/hardware/tools/avr/bin/avrdude"
 CONFIG_PATH="$FIRMWARE_DIR/avrdude.conf"
 HEX_PATH="$FIRMWARE_DIR/uArmPro_V4.5.0_release_20190924.hex"
 
-$AVRDUDE_PATH -C"$CONFIG_PATH" -e -v -patmega2560 -cwiring -P"$PORT" -b115200
-$AVRDUDE_PATH -C"$CONFIG_PATH" -v -patmega2560 -cwiring -P"$PORT" -b115200 -Uflash:w:"$HEX_PATH"
+avrdude -C"$CONFIG_PATH" -e -v -patmega2560 -cwiring -P"$PORT" -b115200
+avrdude -C"$CONFIG_PATH" -v -patmega2560 -cwiring -P"$PORT" -b115200 -Uflash:w:"$HEX_PATH"
